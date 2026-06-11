b05ad90b29cfd3ac2246158f01fbb48b1bf93265
karimmassaoudd <karimmassoud668@gmail.com>
2026-06-11 18:11:18 +0200

    chore: remove unused test artifacts and update gitignore with new exclusion patterns

--- project_x-source/src/main.js
+++
@@ -1,5 +0,0 @@
-import "./styles.css";
-import { RacingGame } from "./systems/RacingGame.js";
-
-const game = new RacingGame(document.querySelector("#game"));
-game.start();
--- test_regex.js
+++
@@ -1,3 +0,0 @@
-const html = `<link rel="stylesheet" crossorigin href="./assets/index-EYUqslG_.css">`;
-const cssMatch = html.match(/<link[^>]+rel=["']stylesheet["'][^>]+href=["']([^"']+)["'][^>]*>/i);
-console.log(cssMatch);
